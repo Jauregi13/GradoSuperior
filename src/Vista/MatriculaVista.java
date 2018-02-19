bef3ef4c802664c1b5b283c1030f712775ce7552
jauregi13 <jon.j1305@gmail.com>
2018-02-20 00:37:12 +0100

    Cambios en MatriculaVista para mostrar bien las matriculas

--- src/Vista/MatriculaVista.java
+++ src/Vista/MatriculaVista.java
@@ -3,19 +3,47 @@ package Vista;
 import Modelo.MatriculaModelo;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
+import Modelo.Alumno;
+import Modelo.AlumnoModelo;
+import Modelo.Asignatura;
+import Modelo.AsignaturaModelo;
 import Modelo.Matricula;
 public class MatriculaVista {
 
 	public static void main(String[] args) {
 		
+		MatriculaModelo matriculaModelo = new MatriculaModelo();
+		ArrayList<Matricula> matriculas = matriculaModelo.listarMatriculas();
 		
 		
 
 	}
 	
 	private void mostrarMatriculas(ArrayList<Matricula> matriculas){
-		MatriculaModelo matriculaModelo = new MatriculaModelo();
+		Iterator<Matricula> i = matriculas.iterator();
+		
+		while(i.hasNext()){
+			Matricula matricula = i.next();
+			
+			mostrarMatricula(matricula);
+			
+			
+		}
+	}
+
+	private void mostrarMatricula(Matricula matricula) {
+		
+		AlumnoModelo alumnoModelo = new AlumnoModelo();
+		Alumno alumno = alumnoModelo.selectPorId(matricula.getId_alumno());
+		
+		AsignaturaModelo asignaturaModelo = new AsignaturaModelo();
+		Asignatura asignatura = asignaturaModelo.selectPorId(matricula.getId_asignatura());
+		
+		System.out.println(alumno.getDni() + " " + alumno.getNombre() + " " + asignatura.getNombre() + " "
+				+ asignatura.getHoras() + " " + matricula.getFecha());
+		
 	}
 
 }
